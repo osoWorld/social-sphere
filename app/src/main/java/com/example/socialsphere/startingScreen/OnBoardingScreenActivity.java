7928b4c4346f259a25149bbd08677f9e560cfc9d
osoWorld <osoworld999@gmail.com>
2024-05-12 00:36:06 +0500

    splash screen done

--- app/src/main/java/com/example/socialsphere/startingScreen/OnBoardingScreenActivity.java
+++ app/src/main/java/com/example/socialsphere/startingScreen/OnBoardingScreenActivity.java
@@ -1,8 +1,6 @@
 package com.example.socialsphere.startingScreen;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.socialsphere.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class OnBoardingScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_on_boarding_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, OnBoardingScreenActivity.class));
-            }
-        },3200);
     }
 }
